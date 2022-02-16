55dbc834324c4d3651c9a5cd71611a35f6aa8898
josehower <josehowerupn@gmail.com>
2022-02-16 11:57:12 +0100

    Set up heroku build configuration

---
+++ ley.config.js
@@ -0,0 +1,10 @@
+const setPostgresDefaultsOnHeroku = require('./util/setPostgresDefaultsOnHeroku');
+setPostgresDefaultsOnHeroku();
+
+const options = {};
+
+if (process.env.NODE_ENV === 'production') {
+  options.ssl = { rejectUnauthorized: false };
+}
+
+module.exports = options;
--- util/database.ts
+++ util/database.ts
@@ -1,7 +1,9 @@
 import camelcaseKeys from 'camelcase-keys';
 import { config } from 'dotenv-safe';
 import postgres from 'postgres';
+import setPostgresDefaultsOnHeroku from './setPostgresDefaultsOnHeroku.js';
 
+setPostgresDefaultsOnHeroku();
 // Read the environment variables from the .env
 // file, which will then be available for all
 // following code
@@ -15,12 +17,20 @@ declare module globalThis {
 // Connect only once to the database
 // https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
 function connectOneTimeToDatabase() {
-  // When in development, connect only once to the database
-  if (!globalThis.postgresSqlClient) {
-    globalThis.postgresSqlClient = postgres();
-  }
-  const sql = globalThis.postgresSqlClient;
+  let sql;
 
+  if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL) {
+    sql = postgres();
+    // Heroku needs SSL connections but
+    // has an "unauthorized" certificate
+    // https://devcenter.heroku.com/changelog-items/852
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
+  } else {
+    if (!globalThis.postgresSqlClient) {
+      globalThis.postgresSqlClient = postgres();
+    }
+    sql = globalThis.postgresSqlClient;
+  }
   return sql;
 }
 
