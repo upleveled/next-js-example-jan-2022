0cd9396071459806ab3b0201307f8619780c66f6
josehower <josehowerupn@gmail.com>
2022-03-11 16:06:56 +0100

    Update max age to match the time on the database

--- util/cookies.ts
+++ util/cookies.ts
@@ -35,7 +35,7 @@ export function createSerializedRegisterSessionTokenCookie(token: string) {
   // check if we are in production e.g. Heroku
   const isProduction = process.env.NODE_ENV === 'production';
 
-  const maxAge = 60 * 10; // 10 minutes
+  const maxAge = 60 * 60 * 24; // 24 hours
 
   return serialize('sessionToken', token, {
     maxAge: maxAge,
