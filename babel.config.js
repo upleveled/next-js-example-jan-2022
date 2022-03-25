d29cb5dc78c99cb4120df8c4644dddc12173eab1
Karl Horky <karl.horky@gmail.com>
2022-03-25 19:38:34 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
+//
+//
+// Warning: Don't use this file anymore! It is now possible to use
+// Emotion with SWC 🎉 https://nextjs.org/docs/advanced-features/compiler#emotion
+// 
+// Check the CSS-in-JS cheatsheet for the latest recommendation
+//
+//
 module.exports = {
   presets: [
     [
